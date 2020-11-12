b277f4f3599e760066328730093f3cd14ad1e7fb
Alexander Danilov <alexander.danilov@jetbrains.com>
2020-11-12 10:51:09 +0300

    change to companion object

--- app/src/main/java/com/jetbrains/danilov/alex/mylibrary/SimpleClass.kt
+++ app/src/main/java/com/jetbrains/danilov/alex/mylibrary/SimpleClass.kt
@@ -1,8 +1,21 @@
 package com.jetbrains.danilov.alex.mylibrary
 
+import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parceler
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class SimpleClass : Parcelable {
+class SimpleClass(
+    val id: Int,
+    val name: String
+) : Parcelable {
+
+    companion object : Parceler<SimpleClass> {
+        override fun create(parcel: Parcel): SimpleClass = SimpleClass(1, "HackedString")
+
+        override fun SimpleClass.write(parcel: Parcel, flags: Int) {
+        }
+
+    }
 }
